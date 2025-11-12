3624234a5b62e1c8ce4d6e471f908d45213d73bf
Thierry Naud <thierrynaud2009@gmail.com>
2025-11-12 18:34:35 -0400

    feat(ui): ajout du badge réseau en ligne / hors-ligne

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import NetworkStatusBadge from "./components/NetworkStatusBadge";
+
+function App() {
+  return (
+    <>
+      <NetworkStatusBadge />
+      {/* le reste de ton application */}
+    </>
+  );
+}
+
+export default App;
