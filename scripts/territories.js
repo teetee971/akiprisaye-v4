6c3fa09b813dfe703a3b1f3869af2f7772b74b20
teetee971 <thierrynaud2009@gmail.com>
2025-11-13 07:47:52 -0400

    Create territories.js

---
+++ scripts/territories.js
@@ -0,0 +1,37 @@
+export const TERRITORIES = {
+  guadeloupe: {
+    code: "guadeloupe",
+    name: "Guadeloupe",
+    lat: 16.265,
+    lon: -61.551,
+    zoom: 11
+  },
+  martinique: {
+    code: "martinique",
+    name: "Martinique",
+    lat: 14.6415,
+    lon: -61.0242,
+    zoom: 11
+  },
+  guyane: {
+    code: "guyane",
+    name: "Guyane",
+    lat: 4.9227,
+    lon: -52.3269,
+    zoom: 8
+  },
+  reunion: {
+    code: "reunion",
+    name: "La Réunion",
+    lat: -21.1151,
+    lon: 55.5364,
+    zoom: 11
+  },
+  mayotte: {
+    code: "mayotte",
+    name: "Mayotte",
+    lat: -12.8275,
+    lon: 45.1662,
+    zoom: 12
+  }
+};
