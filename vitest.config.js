fa58b8ab7aaaa0cc8eaec4c4c75e9053127e4822
teetee971 <thierrynaud2009@gmail.com>
2026-01-25 07:53:31 -0400

    Update vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from "vitest/config";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+
   test: {
     environment: "jsdom",
-
-    // ⛔ AUCUN setupFiles
-    // ⛔ AUCUN setup.ts
-    // ⛔ AUCUN frontend/
-
     globals: false,
 
     include: [
