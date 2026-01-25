034928db36a6e3ba610a56710006bcbb0745614d
teetee971 <thierrynaud2009@gmail.com>
2026-01-25 08:42:46 -0400

    Update vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from "vitest/config";
-import { fileURLToPath } from "node:url";
-import { dirname, resolve } from "node:path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default defineConfig({
-  root: __dirname,
-
   test: {
     environment: "jsdom",
     globals: false,
-    setupFiles: [resolve(__dirname, "src/test/setup.ts")],
     include: ["src/**/*.{test,spec}.{ts,tsx,js,jsx}"],
   },
 });
